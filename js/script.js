ebbfbf19728e92df39e68c5e50ebcabda26aae62
Kamil <isaevkamil029@mail.ru>
2022-06-21 10:13:39 +0300

    add Timer

---
+++ js/script.js
@@ -0,0 +1,88 @@
+window.addEventListener('DOMContentLoaded', function() {
+    'use strict';
+
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+
+
+
+        function hideTabContent(a) {
+            for (let i = a; i < tabContent.length; i++) {
+                tabContent[i].classList.remove('show');
+                tabContent[i].classList.add('hide');
+            }
+        }
+
+        hideTabContent(1);
+
+        function showTabContent(b) {
+            if(tabContent[b].classList.contains('hide')) {
+                tabContent[b].classList.remove('hide');
+                tabContent[b].classList.add('show');
+            }
+        }
+
+        info.addEventListener('click', function(event) {
+            let targetTab = event.target;
+            if (targetTab && targetTab.classList.contains('info-header-tab')) {
+                for (let i = 0; i < tab.length; i++) {
+                    if (targetTab == tab[i]) {
+                        hideTabContent(0);
+                        showTabContent(i);
+                        break;
+                    }
+                }
+            }
+        })
+
+        //Timer
+
+    let deadLine = '2022-06-22';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+        seconds = Math.floor((t/1000) % 60),
+        minutes = Math.floor((t/1000/60) % 60),
+        hours = Math.floor((t/(1000*60*60)));
+        // hours = Math.floor((t/1000/60/60) % 24),
+        // days = Math.floor((t/(1000*60*60*24)))    таймер с отсчетом дней
+
+        return {
+            'total' : t,
+            'hours' : hours,
+            'minutes' : minutes,
+            'seconds' : seconds
+        };
+    }
+
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            function addZero(num) {
+                if(num <= 9) {
+                    return '0' + num
+                } else return num;
+            }
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            if(t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = 0;
+                minutes.textContent = 0;
+                seconds.textContent = 0;
+            }
+        }
+    }
+    setClock('timer', deadLine);
+});
